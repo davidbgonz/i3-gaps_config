86469ab03ead462299145a4dfff43f9f2a32ae8c
David <david.gonz789@gmail.com>
2019-01-05 12:19:38 -0500

    Add option for a dual monitor setup

--- setupMonitors.sh
+++ setupMonitors.sh
@@ -5,6 +5,8 @@ connectedOutputs=$(xrandr | grep " connected" | wc -l)
 
 if [ $connectedOutputs -eq 3 ]; then
 	sh $HOME/.screenlayout/three_monitor.sh
+elif [ $connectedOutputs -eq 2 ]; then
+	sh $HOME/.screenlayout/dual_monitor.sh
 else
 	sh $HOME/.screenlayout/single_monitor.sh
 fi
