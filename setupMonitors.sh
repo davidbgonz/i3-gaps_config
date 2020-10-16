42c1ea6bb13d19f6f7dada650296631206579ffd
David Gonzalez <david.gonz789@gmail.com>
2020-10-16 12:03:42 -0400

    Home setup now consists of an external monitor using a dock
    Will probably need to add some more logic to allow for dual via dock OR
    via HDMI on laptop

--- setupMonitors.sh
+++ setupMonitors.sh
@@ -8,7 +8,7 @@ if [ $connectedOutputs -eq 4 ]; then
 elif [ $connectedOutputs -eq 3 ]; then
 	sh $HOME/.screenlayout/three_monitor.sh
 elif [ $connectedOutputs -eq 2 ]; then
-	sh $HOME/.screenlayout/dual_monitor.sh
+	sh $HOME/.screenlayout/dual_monitor_dock.sh
 else
 	sh $HOME/.screenlayout/single_monitor.sh
 fi
