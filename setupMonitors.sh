f3029aadffef009d42dfa0b8800700028e3608e1
David <david.gonz789@gmail.com>
2021-01-10 17:19:01 -0500

    Updated monitor setup for new monitor and changed background

--- setupMonitors.sh
+++ setupMonitors.sh
@@ -6,10 +6,11 @@ connectedOutputs=$(xrandr | grep " connected" | wc -l)
 if [ $connectedOutputs -eq 3 ]; then
 	sh $HOME/.screenlayout/three_monitor.sh
 elif [ $connectedOutputs -eq 2 ]; then
-	sh $HOME/.screenlayout/dual_monitor.sh
+	sh $HOME/.screenlayout/dual_monitor_dock.sh
 else
 	sh $HOME/.screenlayout/single_monitor.sh
 fi
 
 # Set backgrounds for connected monitors
 #feh --randomize --bg-scale ~/Pictures/material_*
+feh --bg-fill /usr/share/backgrounds/static/jump-ship.jpg
